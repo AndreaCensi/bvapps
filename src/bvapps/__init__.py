7abed1ecfe356c1939e46a7fda04ffbbb5088c68
Andrea Censi <censi@mit.edu>
2014-09-02 08:58:37 -0400

    sep 1

--- src/bvapps/__init__.py
+++ src/bvapps/__init__.py
@@ -8,7 +8,12 @@ def jobs_comptests(context):
     rel = resource_filename("bvapps", "configs")
     dirname = os.path.join(rel, '..', '..', '..', 'bdse1')
     dirname2 = os.path.join(rel, '..', '..', '..', 'bo_app1') # e.g. r_rf_A
-    dirs = [dirname, dirname2, "vehicles.configs"]
+    dirs = [
+        "bootstrapping_olympics.configs", 
+        "vehicles.configs",
+        dirname, 
+        dirname2,
+    ]
     
     from conf_tools import GlobalConfig
     GlobalConfig.global_load_dirs(dirs)
